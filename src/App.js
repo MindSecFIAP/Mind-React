51483e3ba2b2da1cf7325a7ee3441fc5f78d6c9d
Leandroyyy <leandro.cavallari123@gmail.com>
2021-09-12 21:11:16 -0300

    feito tela de chat

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import {Login} from "./pages/Login";
+import {Login} from "./pages/login/Login";
 import {RegisterUser} from "./pages/register/RegisterUser";
 import { RegisterFormado } from './pages/register/RegisterFormado';
 import { RegisterEstagiario } from './pages/register/RegisterEstagiario';
+import {Chat} from './pages/chats/Chat'
+import {ChatVoluntario} from './pages/chats/ChatVoluntario';
 
 import "./App.css";
 
 function App() {
   return (
     <>
- 
     <BrowserRouter>
         <Switch>
+          <Route path="/chatVoluntario" exact component={ChatVoluntario} />
+          <Route path="/chatUser" exact component={Chat} />
           <Route path="/registerUser" exact component={RegisterUser} />
           <Route path="/registerEstagiario" exact component={RegisterEstagiario} />
           <Route path="/registerFormado" exact component={RegisterFormado} />
